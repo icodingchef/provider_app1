b29eee568d9b3875b5561d2747044ff5166de81a
icodingchef <icodingchef@gmail.com>
2023-12-16 22:44:31 +0900

    Initial commit

---
+++ lib/fish_model.dart
@@ -0,0 +1,11 @@
+class Salmon {
+  final String fishName;
+  final int fishNumber;
+  final String fishSize;
+
+  Salmon({
+    required this.fishName,
+    required this.fishNumber,
+    required this.fishSize,
+  });
+}
